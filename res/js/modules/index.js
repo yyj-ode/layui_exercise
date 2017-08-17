362b21758da4996650808b39b327651bf2f7b7cf
yyj-ode <yyj20141112@163.com>
2017-08-17 18:57:28 +0800

    first commit

---
+++ res/js/modules/index.js
@@ -0,0 +1,7 @@
+layui.use(['layer', 'form'], function(){
+    var layer = layui.layer
+        ,form = layui.form();
+
+    layer.msg('Hello World');
+    exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+});
