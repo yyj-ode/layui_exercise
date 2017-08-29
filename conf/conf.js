f8992f269e6c3d77d6f1f219e5c9710586c8c847
yyj-ode <yyj20141112@163.com>
2017-08-29 17:30:13 +0800

    发布通告静态页完成

--- conf/conf.js
+++ conf/conf.js
@@ -1,3 +1,12 @@
+// 本地配置
 // var conf_host = 'http://localhost/yanku/';
+
+
+// 测试服务器配置
 var conf_host = 'http://back.yankushidai.com/index.php/';
+
+
+// 线上服务器配置
+// var conf_host = 'http://rest.yankushidai.com/index.php/';
+
 
